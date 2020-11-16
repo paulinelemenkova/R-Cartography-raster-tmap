bb0a70b00abe7be7262e0511eac5a5c09f6f6ad7
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2020-11-16 14:52:21 +0300

    Add files via upload

--- Peru_SlopeAspect.r
+++ Peru_SlopeAspect.r
@@ -10,40 +10,6 @@ library(RColorBrewer)
 library(sf)
 library(tmap)
 
-############################# -- CROP -- ######################
-DEM <- raster("GEBCO_2019.nc")
-# Crop Red Sea (WESN)
-e <- extent(-85, -70, -20, 1)
-Peru <- crop(DEM, e)
-
-############################# -- COORDINATE SYSTEM -- ######################
-crs(Peru)
-# CRS arguments: +proj=longlat +datum=WGS84 +no_defs
-crs(Peru) <- "+proj=lcc +lat_1=-14 +lat_2=-7 +lon_0=-77 +datum=WGS84"
-crs(Peru)
-s <- trim(Peru)
-
-############################ -- CREATE COLOUR PALETTE -- ############
-blue.col <- colorRampPalette(c("darkblue", "lightblue"))
-#grey.col <- colorRampPalette(c("white", "black"))
-yel.col <- colorRampPalette(c("moccasin", "navajowhite4"))
-palette <- function(x, b1=50, b2=50, r1=-2, r2=-2) {
- mi <- cellStats(x, stat="min")-100
- ma <- cellStats(x, stat="max")+100
- s1 <- unique(round(seq(mi, 0, 0-mi/b1),r1))
- s2 <- unique(round(seq(0, ma, ma/b2),r2))
- s3 <- c(s1, s2[-1])
- x <- list(length(s1)-1, length(s2)-1, s3)
-}
-
-############################# -- VISULIAZTION-- ######################
-PeruMap <- palette(Peru)
-plot(s, col=terrain.colors(256), alpha=NULL,
-    main="Peru region. \nTopographic map: GEBCO 2019",
-    sub="Plotting: R", npretty=4, xlab="N", ylab="E")
-contour(s, add=TRUE)
-#scalebar(1000, xy=c(30, 11), type='bar', divs=3, below = "km")
-
 ############################# -- GET DATA -- ######################
 # Calculate terrain characteristics
 # Elevation data is needed to create a hillshade map. The raster package provides an easy access to the SRTM 90 m resolution elevation data with the getData() function. For example, it is possible to download the elevation data for the whole country of "" using the code below: download the elevation data for the whole country of Slovenia
@@ -52,26 +18,16 @@ alt = getData("alt", country = "Peru", path = tempdir())
 ############################# -- Calculate terrain characteristics: SLOPE AND ASPECT -- ######################
 
 # Hillshade maps are created based on certain terrain characteristics - slope and aspect. Both of them can be calculated with the terrain function and the opt argument set to "slope" or "aspect".
+# A hillshade map can be created using the tmap package. This package builds maps by stacking different data layers. In this case, the first layer is the hillshade object (hill), colored using different levels of gray.
 
 slope = terrain(alt, opt = "slope")
-aspect = terrain(alt, opt = "aspect")
-hill = hillShade(slope, aspect, angle = 40, direction = 270)
-plot(hill)
-
-slope = terrain(RedSea, opt = "slope", unit="radians", neighbors=8)
 plot(slope)
-
-aspect = terrain(RedSea, opt = "aspect")
+aspect = terrain(alt, opt = "aspect")
 plot(aspect)
-
 hill = hillShade(slope, aspect, angle = 40, direction = 270)
 plot(hill)
 
-plot(aspect)
-contour(RedSea, add=TRUE)
-
-############################# -- Create a hillshade map -- ######################
-# A hillshade map can be created using the tmap package. This package builds maps by stacking different data layers. In this case, the first layer is the hillshade object (hill), colored using different levels of gray.
+############################# -- Thematic Mapping -- ######################
 
 # tmaptools::palette_explorer()
 
@@ -200,10 +156,3 @@ hist(hill, maxpixels=100000, plot=TRUE)
 hist(alt, maxpixels=100000, plot=TRUE)
 hist(slope, maxpixels=100000, plot=TRUE)
 hist(aspect, maxpixels=100000, plot=TRUE)
-
-
-############################# -- GRID-- ######################
-e <- as(raster::extent(30, 50, 10, 30), "SpatialPolygons") %>%
-  st_as_sf()
-grid <- st_make_grid(e, cellsize = c(5, 2.5))
-plot(grid, add=TRUE, col = 'grey', pch=3)
