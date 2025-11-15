16df814945e94ec27d795ed6ff70f45050f23baa
Polina Lemenkova <pauline.lemenkova@gmail.com>
2025-11-15 19:10:33 +0300

    Add files via upload

---
+++ 09_hexogram_pop_density.r
@@ -0,0 +1,49 @@
+library(pals)
+require(reshape2)
+#
+library(sf)
+library(cartography)
+# path to the geopackage file embedded in cartography
+path_to_gpkg <- system.file("gpkg/mtq.gpkg", package="cartography")
+# import to an sf object
+mtq <- st_read(dsn = path_to_gpkg, quiet = TRUE)
+# Create a grid layer, cell size area match the median municipality area
+mygrid <- getGridLayer(
+  x = mtq,
+  cellsize = median(as.numeric(st_area(mtq))),
+  var = "POP",
+  type = "hexagonal"
+)
+# Compute population density in people per km2
+mygrid$POPDENS <- 1e6 * mygrid$POP / mygrid$gridarea
+
+# plot municipalities (only the backgroung color is plotted)
+plot(st_geometry(mtq), col = NA, border = NA, bg = "#deffff")
+# Plot the population density
+choroLayer(x = mygrid, var = "POPDENS", method = "geom", nclass=5,
+           col = carto.pal(pal1 = "turquoise.pal", n1 = 5), border = "grey80",
+           lwd = 0.5, legend.pos = "bottomleftextra", add = TRUE,
+           legend.title.txt = "Population Density\n(people per km2)")
+layoutLayer(title = "Population Distribution in Martinique",
+            sources = "Sources: Insee and IGN, 2018",
+            author = paste0("cartography ", packageVersion("cartography")),
+            frame = FALSE, north = FALSE, tabtitle = TRUE,
+            theme = "turquoise.pal")
+# north arrow
+north(pos = "topleft")
+
+plot(st_geometry(mtq), col = pals::ocean.phase(7), border = NA, bg = "#deffff")
+# Plot the population density
+choroLayer(x = mygrid, var = "POPDENS", method = "geom", nclass=20,
+           col = pals::parula(20),
+           lwd = 0.5, legend.pos = "bottomleftextra", add = TRUE,
+           legend.title.txt = "Population Density\n(people per km2)")
+layoutLayer(title = "Population Distribution in Martinique",
+            sources = "Sources: Insee and IGN, 2018",
+            author = paste0("cartography ", packageVersion("cartography")),
+            frame = FALSE, north = FALSE, tabtitle = TRUE,
+            theme = "turquoise.pal")
+# north arrow
+north(pos = "topright")
+
+bquote(Population Density\n(people per~(km^2))
