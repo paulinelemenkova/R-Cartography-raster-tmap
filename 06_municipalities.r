b5b59dd297bf92d61ad91459a4ce839f137dfe76
Polina Lemenkova <pauline.lemenkova@gmail.com>
2025-11-15 21:56:55 +0300

    Add files via upload

--- 06_municipalities.r
+++ 06_municipalities.r
@@ -4,16 +4,20 @@ library(cartography)
 path_to_gpkg <- system.file("gpkg/mtq.gpkg", package="cartography")
 # import to an sf object
 mtq <- st_read(dsn = path_to_gpkg, quiet = TRUE)
+
+nrow(mtq)
+
 # plot municipalities
-plot(st_geometry(mtq), col = "#e4e9de", border = "darkseagreen4",
-     bg = "lightblue1", lwd = 0.5)
+plot(st_geometry(mtq), col = pals::jet(34),
+    border = "goldenrod4",
+    bg = "snow", lwd = 0.5)
 # plot labels
 labelLayer(
   x = mtq,
   txt = "LIBGEO",
   col= "black",
-  cex = 0.7,
-  font = 4,
+  cex = 0.8,
+  font = 1,
   halo = TRUE,
   bg = "white",
   r = 0.1,
@@ -23,9 +27,9 @@ labelLayer(
 # map layout
 layoutLayer(
   title = "Municipalities of Martinique",
-  sources = "Sources: Insee and IGN, 2018",
+  sources = "Sources: Insee and IGN, 2025",
   author = paste0("cartography ", packageVersion("cartography")),
-  frame = FALSE,
+  frame = T,
   north = TRUE,
   tabtitle = TRUE,
   theme = "taupe.pal"
