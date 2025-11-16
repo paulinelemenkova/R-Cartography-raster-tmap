6580af49f8f2569baaa7332b5d9565c87a8f71a7
Polina Lemenkova <pauline.lemenkova@gmail.com>
2025-11-16 11:52:37 +0300

    Add files via upload

--- 03_pencil_style.r
+++ 03_pencil_style.r
@@ -8,15 +8,17 @@ mtq <- st_read(dsn = path_to_gpkg, quiet = TRUE)
 mtq_pencil <- getPencilLayer(
   x = mtq,
   size = 400,
-  lefthanded = F
+  lefthanded = T
 )
 # plot municipalities (only the backgroung color is plotted)
-plot(st_geometry(mtq), col = "white", border = NA, bg = "lightblue1")
+plot(st_geometry(mtq), col = "white", border = "darkseagreen4",
+    bg = "lightcyan", lwd = 0.5)
+
 # plot administrative status
 typoLayer(
   x = mtq_pencil,
   var="STATUS",
-  col = c("aquamarine4", "yellow3","wheat"),
+  col = c("lightslateblue", "mediumorchid1","lightpink"),
   lwd = .7,
   legend.values.order = c("Prefecture",
                           "Sub-prefecture",
@@ -29,12 +31,13 @@ typoLayer(
 plot(st_geometry(mtq), lwd = 0.5, border = "grey20", add = TRUE, lty = 3)
 # labels for a few  municipalities
 labelLayer(x = mtq[mtq$STATUS != "Simple municipality",], txt = "LIBGEO",
-           cex = 0.9, halo = TRUE, r = 0.15)
+           cex = 0.9, halo = T, r = 0.15)
 # title, source, author
-layoutLayer(title = "Administrative Status",
-            sources = "Sources: Insee and IGN, 2018",
+layoutLayer(title = "Administrative status in Martinique",
+            sources = "Sources: Insee and IGN",
             author = paste0("cartography ", packageVersion("cartography")),
-            north = FALSE, tabtitle = TRUE, postitle = "right",
-            col = "white", coltitle = "black")
+            north = T, tabtitle = T, postitle = "left",
+            scale = 5, frame = T,
+            col = "grey85", coltitle = "black")
 # north arrow
 north(pos = "topleft")
